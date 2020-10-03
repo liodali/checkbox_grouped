1c140bcae73baf356ba89031e4f9762fbb637f6f
dali <lionelviperdali@gmail.com>
2020-10-03 16:38:52 +0100

    dev 0.5.1 change simple test for simple_grouped_chips add multiple choice for simple_grouped_chips

--- test/chip_grouped_test.dart
+++ test/chip_grouped_test.dart
@@ -6,63 +6,57 @@ void main() {
   ///coming soon
 
   testWidgets("test simple SimpleGroupedChip", (tester) async {
-    await tester.pumpWidget(Example1());
+    GlobalKey<SimpleGroupedChipsState<int>> chipKey = GlobalKey<SimpleGroupedChipsState<int>>();
+    await tester.pumpWidget(MaterialApp(
+      home: Scaffold(
+        body: SimpleGroupedChips<int>(
+          key: chipKey,
+          itemTitle: ["1", "2", "4", "5"],
+          values: [1, 2, 4, 5],
+          disabledItems: ["2"],
+          selectedColorItem: Colors.red,
+          backgroundColorItem: Colors.white,
+          textColor: Colors.black,
+          selectedTextColor: Colors.white,
+          disabledColor: Colors.grey[200],
+          isMultiple: false,
+        ),
+      ),
+    ));
 //    await tester.tap(find.byType(ChoiceChip).first);
 //    await tester.pump();
-    ChoiceChip cb=tester.widget(find.byType(ChoiceChip).at(1)) as ChoiceChip;
-    ChoiceChip cb2=tester.widget(find.byType(ChoiceChip).at(2)) as ChoiceChip;
-    expect(cb.isEnabled, false);
-    expect(cb2.isEnabled, true);
+    ChoiceChip cb = tester.widget(find.byType(ChoiceChip).at(2)) as ChoiceChip;
+    // ChoiceChip cb2=tester.widget(find.byType(ChoiceChip).at(2)) as ChoiceChip;
+    await tester.tap(find.byWidget(cb));
+    int value = chipKey.currentState.selection();
+    expect(value, 4);
   });
-}
 
-class Example1 extends StatefulWidget {
-  Example1({Key key}) : super(key: key);
-
-  @override
-  _Example1State createState() => _Example1State();
-}
-
-class _Example1State extends State<Example1> {
-  GlobalKey<SimpleGroupedChipsState<int>> chipKey;
-
-  @override
-  void initState() {
-    super.initState();
-    chipKey = GlobalKey<SimpleGroupedChipsState<int>>();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        primaryColor: Colors.blue,
-      ),
+  testWidgets("test multiple selection SimpleGroupedChip", (tester) async {
+    GlobalKey<SimpleGroupedChipsState<int>> chipKey = GlobalKey<SimpleGroupedChipsState<int>>();
+    await tester.pumpWidget(MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("SingleGroupedChip"),
-        ),
-        body: SingleChildScrollView(
-          child: Column(
-            mainAxisSize: MainAxisSize.max,
-            children: <Widget>[
-              Text("Basic"),
-              SimpleGroupedChips<int>(
-                key: chipKey,
-                itemTitle: ["1", "2", "4", "5"],
-                values: [1, 2, 4, 5],
-                disabledItems: ["2"],
-                selectedColorItem: Colors.red,
-                backgroundColorItem: Colors.white,
-                textColor: Colors.black,
-                selectedTextColor: Colors.white,
-                disabledColor: Colors.grey[200],
-                isMultiple: false,
-              ),
-            ],
-          ),
+        body: SimpleGroupedChips<int>(
+          key: chipKey,
+          itemTitle: ["1", "2", "4", "5"],
+          values: [1, 2, 4, 5],
+          preSelection: [2,4],
+          selectedColorItem: Colors.red,
+          backgroundColorItem: Colors.white,
+          textColor: Colors.black,
+          selectedTextColor: Colors.white,
+          disabledColor: Colors.grey[200],
+          isMultiple: true,
         ),
       ),
-    );
-  }
+    ));
+    await tester.pump();
+
+    var values = chipKey.currentState.selection();
+    expect(values, [2,4]);
+    ChoiceChip cb = tester.widget(find.byType(ChoiceChip).at(2)) as ChoiceChip;
+    await tester.tap(find.byWidget(cb));
+    values = chipKey.currentState.selection();
+    expect(values, [2]);
+  });
 }
