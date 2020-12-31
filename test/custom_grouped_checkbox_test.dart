fb61e38d30d27240531219ef9b98da8f8d6d9e5d
dali <lionelviperdali@gmail.com>
2020-12-31 17:35:35 +0100

    #32 modify example and test of custom_grouped_checkbox

--- test/custom_grouped_checkbox_test.dart
+++ test/custom_grouped_checkbox_test.dart
@@ -4,26 +4,55 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-
   testWidgets("test CustomGroupedCheckbox ", (tester) async {
-    await tester.pumpWidget(MaterialApp(
-      home: Scaffold(
-        body: Builder(
-          builder: (ctx){
-            return CustomGroupedCheckbox<int>(
-              itemBuilder: (ctx,index,v){
-                return Text("$index");
-              },
-              itemCount: 10,
-              values: List<int>.generate(10, (i)=>i),
-            );
-          },
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Scaffold(
+          body: Builder(
+            builder: (ctx) {
+              return CustomGroupedCheckbox<int>(
+                itemBuilder: (ctx, index, v) {
+                  return Text("$index");
+                },
+                itemCount: 10,
+                values: List<int>.generate(10, (i) => i),
+              );
+            },
+          ),
         ),
       ),
-    ),);
+    );
     expect(find.byType(Text), findsNWidgets(10));
     expect(find.text("11"), findsNothing);
     await tester.pump();
-
+  });
+  testWidgets("test CustomGroupedCheckbox with global key", (tester) async {
+    GlobalKey<CustomGroupedCheckboxState> _customCheckBoxKey =
+        GlobalKey<CustomGroupedCheckboxState>();
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Scaffold(
+          body: Builder(
+            builder: (ctx) {
+              return CustomGroupedCheckbox<int>(
+                key: _customCheckBoxKey,
+                itemBuilder: (ctx, index, v) {
+                  return Text("$index");
+                },
+                itemCount: 10,
+                isMultipleSelection: true,
+                values: List<int>.generate(10, (i) => i+1),
+              );
+            },
+          ),
+        ),
+      ),
+    );
+    expect(find.byType(Text), findsNWidgets(10));
+    expect(find.text("11"), findsNothing);
+    await tester.pump();
+    await tester.tap(find.byType(Text).at(4));
+    var selectedItems=_customCheckBoxKey.currentState.selection();
+    expect(selectedItems,[5]);
   });
 }
