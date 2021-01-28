51025aee13b639a72ae5c9df76ddce92fb5d78c0
dali <lionelviperdali@gmail.com>
2021-01-28 15:05:35 +0100

    #37 replace State in simpleGroupedChips with GroupState hide State of simpleGroupedChips from export

--- lib/checkbox_grouped.dart
+++ lib/checkbox_grouped.dart
@@ -1,8 +1,9 @@
 library checkbox_grouped;
 
 export 'src/simple_grouped_checkbox.dart' hide SimpleGroupedCheckboxState;
+export 'src/controller/group_controller.dart';
 export 'src/custom_grouped_checkbox.dart';
-export 'src/simple_grouped_chips.dart';
+export 'src/simple_grouped_chips.dart' hide SimpleGroupedChipsState;
 export 'src/simple_grouped_switch.dart';
 export 'src/show_dialog_grouped_checkbox.dart';
 export 'src/list_grouped_checkbox.dart';
