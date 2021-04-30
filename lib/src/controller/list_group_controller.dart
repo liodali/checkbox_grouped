d7f8996ebcc42667e7e758715a77d7674ca68c4c
dali <lionelviperdali@gmail.com>
2021-04-30 23:31:56 +0100

    1.3.1 implement base controller in group_controller and list group controller

--- lib/src/controller/list_group_controller.dart
+++ lib/src/controller/list_group_controller.dart
@@ -1,10 +1,12 @@
+import 'package:checkbox_grouped/src/controller/base_controller.dart';
+
 import '../widgets/list_grouped_checkbox.dart';
 
 /// ListGroupController to manage list of  grouped checkbox/chips/switch
 /// [isMultipleSelectionPerGroup] : (List<bool>) enable multiple selection  in each grouped checkbox.
 /// [initSelectedValues] : (List) A Initialize list of values on each group of checkbox that will be selected in group.
 
-class ListGroupController {
+class ListGroupController implements BaseController{
   final List<bool> isMultipleSelectionPerGroup;
   final List<List<dynamic>> initSelectedValues;
   late ListGroupedCheckboxState _state;
@@ -23,4 +25,26 @@ class ListGroupController {
   void init(ListGroupedCheckboxState state) {
     this._state = state;
   }
+
+  @override
+  void disabledItemsByTitles(List<String> items) {
+    // TODO: implement disabledItemsByTitles
+  }
+
+  @override
+  void disabledItemsByValues(List itemsValues) {
+    // TODO: implement disabledItemsByValues
+  }
+
+  @override
+  void enabledItemsByTitles(List<String> items) {
+    // TODO: implement enabledItemsByTitles
+  }
+
+  @override
+  void enabledItemsByValues(List itemsValues) {
+    // TODO: implement enabledItemsByValues
+  }
+
+
 }
