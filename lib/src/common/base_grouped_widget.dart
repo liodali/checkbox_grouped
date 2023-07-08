5dfc180c201fe3c4a001b9c2c56a4271ef4e7a1c
liodali <lionelviperdali@gmail.com>
2023-07-09 01:16:26 +0200

    feat: add base_simple_group as statefull widget to share some api with SstateGroup

---
+++ lib/src/common/base_grouped_widget.dart
@@ -0,0 +1,28 @@
+import 'package:checkbox_grouped/src/controller/custom_group_controller.dart';
+import 'package:checkbox_grouped/src/controller/group_controller.dart';
+import 'package:flutter/material.dart';
+
+abstract class BaseSimpleGrouped<T> extends StatefulWidget {
+  final GroupController controller;
+  final List<T> values;
+  final List<String> itemsTitle;
+  final List<T> disableItems;
+
+  const BaseSimpleGrouped({
+    Key? key,
+    required this.controller,
+    required this.values,
+    required this.itemsTitle,
+    this.disableItems = const [],
+  });
+}
+
+abstract class BaeCustomGrouped<T> extends StatefulWidget {
+  final CustomGroupController controller;
+  const BaeCustomGrouped({
+    Key? key,
+    required this.controller,
+  });
+}
+
+abstract class BaeListGrouped extends StatefulWidget {}
